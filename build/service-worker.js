3975550383ec2f465b16b85c2cfc76b5ea2a9554
Xianyin Hu <xianyinhu@XianyinnoMacBook-Air.local>
2022-07-04 02:20:17 +0900

    re build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.136958d15af1950a4571b17de94f433a.js"
+  "/ATMT_Web/precache-manifest.9fb42f5fbafbf49e954bc7ba47126a20.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/ATMT_Web/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
