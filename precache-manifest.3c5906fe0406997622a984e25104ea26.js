56af82e05b9528d6c9db8e4141f65f1a8c05ed4c
Xianyin Hu <xianyinhu@XianyinnoMacBook-Air.local>
2022-07-04 02:24:08 +0900

    Updates

--- precache-manifest.3c5906fe0406997622a984e25104ea26.js
+++ precache-manifest.3c5906fe0406997622a984e25104ea26.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "f1a48b23788abc6c3d90",
+    "revision": "bd15f68cf75437b056bd",
     "url": "/ATMT_Web/static/css/main.9ff33704.chunk.css"
   },
   {
-    "revision": "f1a48b23788abc6c3d90",
-    "url": "/ATMT_Web/static/js/main.f1a48b23.chunk.js"
+    "revision": "bd15f68cf75437b056bd",
+    "url": "/ATMT_Web/static/js/main.bd15f68c.chunk.js"
   },
   {
     "revision": "c873f29189a9d1efb37c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/ATMT_Web/static/js/runtime~main.ed5f5729.js"
   },
   {
-    "revision": "93c72bca60415dc1fcc5621ded547892",
+    "revision": "dc5d1b7b81acd1e8ca0ef9f95c3d10f8",
     "url": "/ATMT_Web/index.html"
   }
 ];
