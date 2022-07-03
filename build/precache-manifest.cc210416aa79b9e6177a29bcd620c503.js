199b3677c0a5a1f7c474072fd677b351385dc3ba
Xianyin Hu <xianyinhu@XianyinnoMacBook-Air.local>
2022-07-04 01:41:58 +0900

    add build folder

---
+++ build/precache-manifest.cc210416aa79b9e6177a29bcd620c503.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00dbb0add3217e7fb4ae",
+    "url": "/ATMT_Web/static/css/main.9ff33704.chunk.css"
+  },
+  {
+    "revision": "00dbb0add3217e7fb4ae",
+    "url": "/ATMT_Web/static/js/main.00dbb0ad.chunk.js"
+  },
+  {
+    "revision": "c873f29189a9d1efb37c",
+    "url": "/ATMT_Web/static/js/1.c873f291.chunk.js"
+  },
+  {
+    "revision": "ed5f57293722c42877be",
+    "url": "/ATMT_Web/static/js/runtime~main.ed5f5729.js"
+  },
+  {
+    "revision": "801646e137e1e6bca6ac5f04ad270ff7",
+    "url": "/ATMT_Web/index.html"
+  }
+];
